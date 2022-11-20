14e9cfd98132fe6ce49fa7e0650feacbde9b2faf
marlloncsoares <marlloncsoares@gmail.com>
2022-11-20 00:56:27 -0300

    feat: adiciona layout inicial para dashboard

---
+++ src/presentation/layouts/dashboard/DashboardLayout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const DashboardLayout: React.FC = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default DashboardLayout;

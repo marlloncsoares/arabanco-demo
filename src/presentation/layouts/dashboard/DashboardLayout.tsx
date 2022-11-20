2fdaf98096a75beb9ceb579c361b29adf30d56b6
marlloncsoares <marlloncsoares@gmail.com>
2022-11-20 02:18:31 -0300

    refactor: faz melhorias no DashboardLayout

--- src/presentation/layouts/dashboard/DashboardLayout.tsx
+++ src/presentation/layouts/dashboard/DashboardLayout.tsx
@@ -1,10 +1,45 @@
+import { themeCustom } from "@core/presentation/theme";
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+
+const materialTheme = createTheme({
+  palette: {
+    primary: {
+      main: themeCustom.colors.primary,
+    },
+  },
+});
 
 const DashboardLayout: React.FC = () => {
   return (
     <>
-      <Outlet />
+      <ThemeProvider theme={materialTheme}>
+        <CssBaseline />
+        <Box
+          maxWidth="lg"
+          sx={{
+            backgroundColor: (theme) => theme.palette.primary.main,
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            maxWidth: "100vw",
+            width: "100vw",
+            height: "100vh",
+            maxHeight: "100vh",
+          }}
+        >
+          <Container maxWidth="lg" fixed>
+            <Outlet />
+          </Container>
+        </Box>
+      </ThemeProvider>
     </>
   );
 };
